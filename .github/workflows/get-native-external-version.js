61035e4ec96aa94aaa4b50503d2ef4d99a5c1937
intrigus <intrigus@users.noreply.github.com>
2023-04-03 18:01:39 +0200

    Update get-native-external-version.js

--- .github/workflows/get-native-external-version.js
+++ .github/workflows/get-native-external-version.js
@@ -5,5 +5,5 @@ const configJsonPath = path.join(__dirname, '../../native/external-config.json')
 const configData = fs.readFileSync(configJsonPath);
 const config = JSON.parse(configData);
 const externalVersion = config.from.checkout;
-console.log("LOLOL");
+console.log("LOLOLOL");
 console.log(externalVersion);
